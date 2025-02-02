e95e2c61f800a7bc47d22eb28268580240071281
Purbarun <purbarunmondal@gmail.com>
2025-02-02 16:02:46 +0530

    Update projectDate validation to ensure valid date format as a string

--- utils/validation_schema/projectValidationSchema.js
+++ utils/validation_schema/projectValidationSchema.js
@@ -29,9 +29,9 @@ export const projectValidationSchema = z.object({
       message: "Project description cannot be empty",
     }),
 
-  projectDate: z.string({
-    message: "Project date must be a valid date",
-  }),
+  projectDate: z
+    .string()
+    .refine((val) => !isNaN(Date.parse(val)), "Invalid date format."),
 
   projectPeriod: z
     .number({
