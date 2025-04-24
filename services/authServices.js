a7d892b7a9aa4e8cad20c798a3f1cfcd2d5f33c9
Purbarun <purbarunmondal@gmail.com>
2025-04-24 11:35:15 +0530

    Fix login service URL and update user profile details API endpoint

--- services/authServices.js
+++ services/authServices.js
@@ -4,7 +4,9 @@ import { logger } from "../utils/logger.js";
 export const loginService = async (credentials) => {
   const AUTH_SERVICE_BASE_URL = process.env.AUTH_SERVICE_BASE_URL;
   try {
-    const response = await axios.get(`${AUTH_SERVICE_BASE_URL}/${credentials}`);
+    const response = await axios.get(
+      `${AUTH_SERVICE_BASE_URL}/app-login/${credentials}`
+    );
     return response.data;
   } catch (error) {
     logger.error(error.response?.data?.response || "Auth service error");
